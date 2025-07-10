8b08f77303f9dcaa13bf0090d6b54d1aafcf5b7b
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2025-07-10 10:23:23 +0800

    Update build sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir cmake-build-debug
 cd cmake-build-debug
 cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -S .. -B .
 cd ..
-cmake --build cmake-build-debug --target all -- -j 14
-cmake --build cmake-build-debug --target all -- -j 14
+cmake --build cmake-build-debug --target all -- -j 8
+cmake --build cmake-build-debug --target all -- -j 8
